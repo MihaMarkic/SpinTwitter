9f7e6b47aad3aed45900fbbb7f8e4ab5905021fe
Miha Markič <miha@rthand.com>
2019-04-09 08:52:51 +0200

    Updates unit test

--- Source/SpinTwitter.Test/Services/Implementation/SpinFeedTest.cs
+++ Source/SpinTwitter.Test/Services/Implementation/SpinFeedTest.cs
@@ -21,6 +21,7 @@ namespace SpinTwitter.Test.Services.Implementation
                 Assert.That(actual.Value.Length, Is.EqualTo(30));
                 var seven = actual.Value[6];
                 Assert.That(seven.Caption, Is.EqualTo("Reševanje obolelih in helikopterski prevozi med bolnišnicami"));
+                Assert.That(seven.Text, Is.EqualTo("Ob 12.21 je posadka helikopterja SV, v spremstvu ekipe HENMP iz Ljutomera prepeljala obolelo osebo v UKC Maribor. Kraj pristanka helikopterja so zavarovali gasilci PGD Ljutomer."));
                 Assert.That(seven.WgsLat, Is.EqualTo(46.525m));
                 Assert.That(seven.WgsLon, Is.EqualTo(16.191m));
                 Assert.That(seven.ReportDate, Is.EqualTo(new DateTimeOffset(2019, 04, 08, 12,50, 15, 163, TimeSpan.FromHours(2))));
